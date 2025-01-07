78a2ac0f243aa764539c66250f68e6a96f407fd7
Nadeeshan R Wijesinghe <119121943+NadeeshanRW@users.noreply.github.com>
2025-01-07 19:58:59 +0530

    add CPU, memory ,disk details

--- server.sh
+++ server.sh
@@ -1,17 +1,45 @@
-#! /bin/bash
+#! /bin/bash 
 
 
-REPORT="/home/nadiya/Desktop/report.txt"
+ REPORT="/home/nadiya/Desktop/report.txt"
 
 date=$(date "+%y-%m-%d")
 time=$(date "+%H:%M:%S")
 
  
-echo "===========SEVER REPORT============="  >>report.txt
+echo "===========SEVER REPORT=============" 
 
-echo "DATE OF REPORT :- $date"  >>Report.txt
-echo "TIME OF THE REPORT :- $time"  >>Report.txt
+echo "DATE OF REPORT :- $date"     
+echo "TIME OF THE REPORT :- $time"  
+
+echo =============================================== 
+
+echo "cpu  Usage: "
+
+top -bn1 | grep "Cpu(s)" | awk '{print  $2 + $4 "%"}'
+
+echo=================================================
+echo
+echo "uptime: $(uptime -p)"
+echo 
+echo=================================================
+echo
+
+echo "Memory  Usage: " 
+echo
+
+free -h 
+
+echo =================================================
+
+
+echo "Disk Usage: " 
+df -h
+echo "disk space: "
+
+df --total -h | grep "total"
 
 
 
-echo =============================================== >>report.txt
+
+
