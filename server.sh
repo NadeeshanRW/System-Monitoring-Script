384d9995d20528c0dff5473a440fb36fb6a193e0
Nadeeshan R Wijesinghe <119121943+NadeeshanRW@users.noreply.github.com>
2025-01-07 17:32:42 +0530

    Add files via upload

---
+++ server.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+
+REPORT="/home/nadiya/Desktop/report.txt"
+
+date=$(date "+%y-%m-%d")
+time=$(date "+%H:%M:%S")
+
+ 
+echo "===========SEVER REPORT============="  >>report.txt
+
+echo "DATE OF REPORT :- $date"  >>Report.txt
+echo "TIME OF THE REPORT :- $time"  >>Report.txt
+
+
+
+echo =============================================== >>report.txt
